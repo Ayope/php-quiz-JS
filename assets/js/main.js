8e032cc8fd54bed8c89ec6c6dc6496819b47f809
Ayoub El Ayouk <ayoubelayouk1@gmail.com>
2022-12-29 14:22:30 +0100

    adding timer and show what correct answer features

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,8 +17,12 @@ const options = document.querySelector(".answers") ;
 
 var progBar = document.getElementById("progessBar");
 
+let htmlTimer = document.getElementById('timer');
+
 let que_count = 0;
 var score = 0;
+let counter;
+let timeValue = 30;
 
 function shuffle(array){
     for(var i = array.length-1 ; i>0; i--){
@@ -32,21 +36,28 @@ function shuffle(array){
 
 shuffle(questions);
 
-function showQuest(index){
+function showQuest(index){  
+    
     questionNum.innerText = que_count + 1;
+    
     question.innerText = questions[index].question;
-
+    
     firstAnswer.innerText = questions[index].choice1;
-    firstAnswer.style.backgroundColor = "#ccc"
+    firstAnswer.style.backgroundColor = "#ccc";
+    
     secondAnswer.innerText = questions[index].choice2;
-    secondAnswer.style.backgroundColor = "#ccc"
+    secondAnswer.style.backgroundColor = "#ccc";
+    
     thirdAnswer.innerText = questions[index].choice3;
-    thirdAnswer.style.backgroundColor = "#ccc"
+    thirdAnswer.style.backgroundColor = "#ccc";
+    
     fourthAnswer.innerText = questions[index].choice4;
-    fourthAnswer.style.backgroundColor = "#ccc"
-    for(let i = 0; i < options.children.length; i++){
-        options.children[i].classList.remove("disable")
-    }
+    fourthAnswer.style.backgroundColor = "#ccc";
+    
+    enableBtn();
+    
+    clearInterval(counter);
+    startTimer(timeValue);
 }
 
 showQuest(que_count);
@@ -54,54 +65,76 @@ showQuest(que_count);
 function next(answer){
     value = answer.getAttribute('value');
 
-    if(que_count === questions.length - 1){
+        if(que_count === questions.length - 1){
             if(questions[que_count].answer == value){
                 score++;
-                answer.style.backgroundColor = 'green' ;
+                answer.style.backgroundColor = '#2ee01e' ;
                 checkBulletProgbar('correct');
             }else{
-                answer.style.backgroundColor = 'red' ; 
+                answer.style.backgroundColor = '#EA4444' ; 
                 checkBulletProgbar('wrong');
+                showCorrect();
             }
         
-            for(let i = 0; i < options.children.length; i++){
-                options.children[i].classList.add("disable")
-            }
+            disableBtn()
 
             setTimeout(function(){
-                progress2bar.classList.remove('active');
-                progress2bar.classList.add('completed');
-        
-                progress3bar.classList.add('completed');
-        
-                mainQuestPart.style.display = 'none';
-                scorePart.style.display = 'flex';
-        
-                scoreNum.innerHTML = '<h2>'+(score/10)*100+'%</h2>'
-
-                progBar.style.display = 'none';
+                showScore();
             }, 1500)
 
-   
-            //the last question doesn't show the green and the red answers
         } else{
             if(questions[que_count].answer == value){
                 score++;
-                answer.style.backgroundColor = 'green' ;
+                answer.style.backgroundColor = '#2ee01e' ;
                 checkBulletProgbar('correct');
+                clearInterval(counter);
             }else{
-                answer.style.backgroundColor = 'red' ; 
+                answer.style.backgroundColor = '#EA4444' ; 
+                showCorrect();
                 checkBulletProgbar('wrong');
+                clearInterval(counter);
             }
         
-            for(let i = 0; i < options.children.length; i++){
-                options.children[i].classList.add("disable")
-            }
+            disableBtn()
 
             setTimeout(function(){
                 que_count++;
                 showQuest(que_count);
             }, 1000)
+        }
+}
+
+function showScore(){
+    progress2bar.classList.remove('active');
+    progress2bar.classList.add('completed');
+
+    progress3bar.classList.add('completed');
+
+    mainQuestPart.style.display = 'none';
+    scorePart.style.display = 'flex';
+
+    scoreNum.innerHTML = '<h2>'+(score/10)*100+'%</h2>'
+
+    progBar.style.display = 'none';
+}
+
+function disableBtn(){
+    for(let i = 0; i < options.children.length; i++){
+        options.children[i].classList.add("disable");
+    }
+}
+
+function enableBtn(){
+    for(let i = 0; i < options.children.length; i++){
+        options.children[i].classList.remove("disable")
+    }
+}
+
+function showCorrect(){
+    for(let i = 0; i < options.children.length; i++){
+        if(questions[que_count].answer == options.children[i].getAttribute('value')){
+            options.children[i].style.backgroundColor = '#2ee01e';
+        }
     }
 }
 
@@ -121,3 +154,31 @@ addBulletProgbar();
 function checkBulletProgbar(marktype){
     progBar.children[que_count].classList.add(marktype);
 }
+
+
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        htmlTimer.textContent = time;
+        time--;
+
+        if(time < 0){
+            showCorrect();
+            if(que_count === questions.length - 1){
+                showCorrect();
+                setTimeout(()=>{showScore()}, 1000)   
+            }
+
+            checkBulletProgbar('wrong');
+
+            setTimeout(()=>{
+                que_count++;
+                showQuest(que_count);
+            }, 1000);
+
+            time = timeValue;
+
+        }
+    } 
+}
+
